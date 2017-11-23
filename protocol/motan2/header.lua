f8e00955450dc798655d79a21207abcf6331cd14
idevz <zhoujing_k49@163.com>
2017-11-24 00:18:25 +0800

    add todo for unpack

--- protocol/motan2/header.lua
+++ protocol/motan2/header.lua
@@ -38,7 +38,7 @@ function _M.pack_header(self)
     header_buffer = header_buffer .. utils.msb_numbertobytes(self.msg_type, 1)
     header_buffer = header_buffer .. utils.msb_numbertobytes(self.version_status, 1)
     header_buffer = header_buffer .. utils.msb_numbertobytes(self.serialize, 1)
-    -- @TODO big num
+    -- @TODO big num pack to bytes
     header_buffer = header_buffer .. utils.msb_numbertobytes(self.request_id, 8)
     -- header_buffer = header_buffer .. self.request_id
     -- local upper, lower = utils.split2int(self.request_id)
