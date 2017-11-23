77aaf9f9f0dce20ba8808c13f31278378770209a
idevz <zhoujing_k49@163.com>
2017-11-23 23:42:55 +0800

    update demo

--- examples/motan-service/status.lua
+++ examples/motan-service/status.lua
@@ -3,7 +3,7 @@
 
 local setmetatable = setmetatable
 local share_motan = ngx.shared.motan
--- local utils = require "motan.utils"
+local utils = require "motan.utils"
 local singletons = require "motan.singletons"
 local serialize = require "motan.serialize.simple"
 local bit = bit
@@ -25,20 +25,6 @@ function _M.new(self, opts)
     return setmetatable(status, mt)
 end
 
-local function msb_stringtonumber(str)
-    local function _b2n(num, digit, ...)
-        if not digit then return num end
-        return _b2n(num * 256 + digit, ...)
-    end
-    return _b2n(0, string.byte(str, 1, -1))
-end
-
-local function toxxnumber(int_str)
-    local bt_arr = {string.byte( int_str, 1, -1)}
-    local x = bor(bor(bor(bor(bor(bor(bor(bor(lshift(bt_arr[1], 56),lshift(bt_arr[2], 48)), lshift(bt_arr[3], 40)), lshift(bt_arr[4], 32)), lshift(bt_arr[5], 24)), lshift(bt_arr[6], 16)), lshift(bt_arr[6], 16)), lshift(bt_arr[7], 8)),bt_arr[8])
-    return x
-end
-
 function _M.show_batch(self, opts)
     local idevz = share_motan:get("idevz") or 0
     share_motan:set("idevz", idevz + 1)
@@ -48,11 +34,9 @@ function _M.show_batch(self, opts)
     -- local rpc_res_tmp = client:hello("<-----Motan")
     -- local rpc_res = serialize.deserialize(rpc_res_tmp.body)
 
-    local bigint = msb_stringtonumber(opts.bigint)
-
-ngx.log(ngx.ERR, "\n\n", bigint , "\n\n")
-    return sprint_r(opts) .. num .. "\n\n" .. table.concat( {string.byte(opts.bigint, 1, -1)}, ", ") .. "\n" .. toxxnumber(opts.bigint)
-    -- return sprint_r(opts) .. num .. int64 .. "\n" .. tostring(int64)
+    local big_int_num = utils.unpack_request_id(opts.bigint)
+    local big_int_byte = utils.pack_request_id(big_int_num)
+    return sprint_r(utils.unpack_request_id(big_int_byte))
     
     -- if type(opts) == "table" then
     --     if not opts.name then
