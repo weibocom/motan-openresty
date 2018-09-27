75db542d4a7564a2ddcf4cecf80cf212d1bb7121
yangjian9 <yangjian9@staff.weibo.com>
2018-09-27 15:07:42 +0800

    sock 多次建联, 导致tcp连接占用涨满

--- lib/motan/endpoint/motan.lua
+++ lib/motan/endpoint/motan.lua
@@ -95,7 +95,7 @@ function _M.connect(self)
             return nil, "motan endpoint failed connect to weibo mesh, and also couldn't get the snapshots."
         end
     end
-    return sock:connect(self.url.host, self.url.port)
+    return ok, err
 end
 
 function _M.call(self, req)
